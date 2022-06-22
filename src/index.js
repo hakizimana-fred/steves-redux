69a3defb3e1b50cd53b60ed6abdc0367ea770158
hakifred <hakifred20@gmail.com>
2022-06-22 20:54:59 +0300

    first redux meetup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, combineReducers }  from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+
+
+
+const changeUser = (name) => ({
+  type: 'CHANGE_NAME',
+  payload: name
+})
+
+export const userAction = (name) => (dispatch) => { 
+ dispatch(changeUser(name))
+}
+
+
+
+
+const user = {
+  name: 'steve',
+  email: 'steve@gmail.com',
+  phone: '055555'
+}
+
+const initialState = {
+  itemName: 'phone',
+  brand: 'Samsung'
+}
+function userReducer(state = user, action) {
+  switch(action.type) {
+    case 'GET_NAME':
+      return state.name
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.payload
+      }
+
+    default:
+      return state
+  }
+}
+
+function shoppingReducer(state = initialState, action) {
+  switch(action.type) {
+    case 'GET_ITEM':
+      return state.item
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: shoppingReducer
+})
+
+const store =  createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+       <App />
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
